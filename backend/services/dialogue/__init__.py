ad5954a1e058a4582ba0b0b4ceb5885e63482e71
mandrilz <mandrilov@gmail.com>
2026-01-14 19:13:00 +0300

    Delete backend directory

--- backend/services/dialogue/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Пакет для управления диалогом.
-Содержит единый orchestrator для координации NLU, slot filling и генерации ответов.
-"""
-from services.dialogue.manager import DialogueManager, DialogueTurn, DialogueResult
-
-__all__ = ["DialogueManager", "DialogueTurn", "DialogueResult"]
